28aefb2ce088b99b831d452acca07a3f3a54bdd6
Rishabh Joshi <rishabh9@gmail.com>
2015-10-27 11:54:42 +0530

    Add new API to UserDao - getUserByUsername(username)

--- user-mgmt/src/main/java/in/notwork/temper/user/db/dao/iface/UserDao.java
+++ user-mgmt/src/main/java/in/notwork/temper/user/db/dao/iface/UserDao.java
@@ -6,4 +6,12 @@ import in.notwork.temper.user.db.dbo.objects.User;
  * @author rishabh.
  */
 public interface UserDao extends GenericDao<User, Long> {
+
+    /**
+     * Find user by its username.
+     *
+     * @param username The username of the User.
+     * @return User.
+     */
+    User getUserByUsername(String username);
 }
--- user-mgmt/src/main/java/in/notwork/temper/user/db/dao/impl/UserDaoImpl.java
+++ user-mgmt/src/main/java/in/notwork/temper/user/db/dao/impl/UserDaoImpl.java
@@ -2,6 +2,9 @@ package in.notwork.temper.user.db.dao.impl;
 
 import in.notwork.temper.user.db.dao.iface.UserDao;
 import in.notwork.temper.user.db.dbo.objects.User;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -20,4 +23,15 @@ public class UserDaoImpl extends GenericDaoImpl<User, Long> implements UserDao {
         super(User.class);
     }
 
+    public User getUserByUsername(String username) {
+        Session session = getSession();
+        Criteria criteria = session.createCriteria(User.class);
+        if (isEntityArchivable()) {
+            // Don't include archived records.
+            criteria.add(Restrictions.eq("archived", false));
+        }
+        User user = (User) criteria.add(Restrictions.eq("username", username))
+                .uniqueResult();
+        return user;
+    }
 }
